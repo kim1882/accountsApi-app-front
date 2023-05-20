9b98170d9c9a4dce3d71fe30da7b4ae23c9f70cc
Guadalupe Estrada <kim1882@gmail.com>
2023-05-19 23:41:53 -0600

    add some tests

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,8 +1,19 @@
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import Home from "./page";
 import { renderWithProviders } from "../../utils/test-utils";
+import * as accountServices from "../services/accounts";
+import { IAccount } from "@/types/Accounts";
+
+jest.mock("../services/accounts");
+
+const accounts: IAccount[] = [
+  { id: "1", name: "My account", transactions: [] },
+];
 
 describe("Home", () => {
+  beforeEach(() => {
+    accountServices.loadAccountsService.mockResolvedValue(accounts);
+  });
   it("renders the home page", () => {
     renderWithProviders(<Home />);
 
@@ -11,5 +22,22 @@ describe("Home", () => {
     });
 
     expect(heading).toBeInTheDocument();
+
+    expect(accountServices.loadAccountsService).toHaveBeenCalled();
+  });
+
+  it("opens create new account dialog", () => {
+    renderWithProviders(<Home />);
+
+    const createAccountBtn = screen.getByRole("button", {
+      name: /New Account/i,
+    });
+
+    fireEvent.click(createAccountBtn);
+    const dialogHeader = screen.getByRole("heading", {
+      name: /Create New Account/i,
+    });
+
+    expect(dialogHeader).toBeInTheDocument();
   });
 });
